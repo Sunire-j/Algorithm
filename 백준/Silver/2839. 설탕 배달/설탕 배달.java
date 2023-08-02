36c305161140bb82ef5202a4c475eb02b6c14805
Sunire <81051472+Sunire-j@users.noreply.github.com>
2023-08-03 08:56:01 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -2,30 +2,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import static java.lang.System.exit;
-
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int A = Integer.parseInt(br.readLine());
-        int kg5 = A/5;
 
-        if(A%5==0) {
-            System.out.println(A/5);
-            exit(0);
-        }
-        A = A%5;
-        for(; kg5>=0; kg5--){
-            if(A%3==0){
-                System.out.println(kg5+A/3);
-                break;
-            }
-            if(kg5==0) {
-                System.out.println(-1);
-                break;
-            }
-            A +=5;
-        }
+        if (A == 4 || A == 7) System.out.println(-1);
+        else if (A % 5 == 0) System.out.println(A / 5);
+        else if (A % 5 == 1 || A % 5 == 3) System.out.println(A / 5 + 1);
+        else if (A % 5 == 2 || A % 5 == 4) System.out.println(A / 5 + 2);
+
     }
 }
