af988585726d7b9ceed4e9b14b85b2ccd71eec03
Sunire <81051472+Sunire-j@users.noreply.github.com>
2023-08-20 17:21:34 +0900

    [Bronze V] Title: A+B - 8, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -8,9 +8,11 @@ public class Main {
         int input = Integer.parseInt(br.readLine());
         for (int i = 0; i < input; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+            StringBuilder sb = new StringBuilder("Case #");
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            bw.write("Case #" + (i + 1) + ": " + a + " + " + b + " = " + (a + b) + "\n");
+            sb.append(i+1).append(": ").append(a).append(" + ").append(b).append(" = ").append(a+b).append("\n");
+            bw.write(String.valueOf(sb));
         }
         bw.flush();
         bw.close();
