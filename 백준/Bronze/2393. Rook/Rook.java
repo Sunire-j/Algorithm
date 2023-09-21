aeead785ce2420c90a1e271e578187d6a47a5e91
Sunire <81051472+Sunire-j@users.noreply.github.com>
2023-09-22 07:46:45 +0900

    [Bronze V] Title: Rook, Time: 124 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
+        bw.flush();
+        bw.close();
+    }
+}
+
