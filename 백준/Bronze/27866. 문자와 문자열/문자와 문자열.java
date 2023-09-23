1009d4d8c2e645e6e5f1998a56105a8aa77d83f3
Sunire <81051472+Sunire-j@users.noreply.github.com>
2023-09-23 16:54:02 +0900

    [Bronze V] Title: 문자와 문자열, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String temp = br.readLine();
+        bw.write(temp.charAt(Integer.parseInt(br.readLine())-1));
+        bw.flush();
+        bw.close();
+    }
+}
